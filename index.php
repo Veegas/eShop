76110cc195e1e8a5bc50a565a596b73042064ea9
omarkamal95 <omarkamal95@gmail.com>
2015-09-27 17:20:05 +0200

    finished templates

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<h1> 
-<?php echo "WOWOWOW"; ?>
-</h1>
+<?php    
+    // load up your config file
+    require_once("./resources/config.php");
+     
+    require_once(TEMPLATES_PATH . "/header.php");
+?>
+<div id="container">
+    <div id="content">
+        asdasd
+    </div>
+    <?php
+        require_once(TEMPLATES_PATH . "/authentication.php");
+    ?>
+</div>
+<?php
+    require_once(TEMPLATES_PATH . "/footer.php");
+?>
