b101a9020df2e5afc184e6c2300639cd7e36fa3f
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-09-29 16:27:09 +0200

    Added purchase relation to db and connected pages to it

--- eShopDB.sql
+++ eShopDB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 29, 2015 at 12:00 AM
+-- Generation Time: Sep 29, 2015 at 04:25 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `Product` (
   `price` float NOT NULL,
   `quantity` int(11) NOT NULL,
-  `decription` text NOT NULL,
+  `description` text NOT NULL,
   `ID` int(11) NOT NULL,
   `photo` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -49,6 +49,19 @@ CREATE TABLE IF NOT EXISTS `User` (
   `avatar` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User_Product_Purchase`
+--
+
+CREATE TABLE IF NOT EXISTS `User_Product_Purchase` (
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -66,6 +79,13 @@ ALTER TABLE `User`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `User_Product_Purchase`
+--
+ALTER TABLE `User_Product_Purchase`
+  ADD KEY `Product_foreign` (`product_id`),
+  ADD KEY `user_foreign` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -79,6 +99,17 @@ ALTER TABLE `Product`
 --
 ALTER TABLE `User`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `User_Product_Purchase`
+--
+ALTER TABLE `User_Product_Purchase`
+  ADD CONSTRAINT `User_Product_Purchase_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `User_Product_Purchase_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `Product` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
