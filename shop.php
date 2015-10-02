2b429c39f24f43a7a8ecb8dcb809fabf0fffab0a
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-10-02 23:36:05 +0200

    Added cart

--- shop.php
+++ shop.php
@@ -1,13 +1,14 @@
 <?php    
     // load up your config file
     require_once("./resources/config.php");
-     
+         session_start();
+
     require_once(TEMPLATES_PATH . "/header.php");
     // require_once(TEMPLATES_PATH . "/authentication.php");
 ?>
 
 <script>
-        var signIn = function signIn(event) {
+    var signIn = function signIn(event) {
             $("#modal1").openModal();
     }
 </script>
@@ -24,38 +25,47 @@
         while($row = mysqli_fetch_assoc($result)) {
 ?>  
     <form class="buy-form" action="buy.php" method="post" onsubmit=<?php if (!isset($_SESSION["user"])) { echo "'event.preventDefault(); signIn();'"; } ?>>
-        <div class="col s4 md4 l4 products-item">
-        <input type="text" name="item-id" value="<?php echo $row["ID"]; ?>" hidden>
-            <img class="materialboxed products-img" data-caption="<?php  echo $row["description"] ?>" src="<?php echo $row['photo']; ?>" alt="" >
-            <table class="product-caption">
-                <tr>
-                    <td class="left-align">
-                        <?php echo $row["product_name"] ?>
-                    </td>
-                    <td class="right-align">
-                        <?php echo $row["price"]; ?>$
-                    </td>
-                </tr>
-                <tr>
-                    <td class="left-align">
-                        <?php echo $row["quantity"] ?>
+        <div class="col s4 md4 l4 ">
+        <input class="item-id-input" type="text" name="item-id" value="<?php echo $row["ID"]; ?>" hidden>
+
+            <div class="card products-item">
+            <div class="card-image">
+            <img class="activator products-img" data-caption="<?php  echo $row["description"] ?>" src="<?php echo $row['photo']; ?>" alt="" >
+            </div>
+
+            <div class="card-content ">
+              <span class="card-title black-text"> <?php echo $row["product_name"] ?> </span>
+              <span class="card-title black-text right"> <?php echo $row["price"]; ?>$ </span>
+            </div>
+
+            <div class="card-reveal">
+              <span class="card-title black-text"> <?php echo $row["product_name"] ?> </span>
+              <p> <?php echo $row["description"] ?> </p>
+                                
+            </div>
+            <div class="card-action">
+                    <tr>
+                    <td class="left">
+                            <button class="btn-floating waves-effect waves-light left cart-btn" type="button" onclick="addToCart(event);">
+                                <i class="material-icons right">add_shopping_cart</i>
+                            </button>
                     </td>
 
-                    <td class="right-align">
                         <?php if ($row["quantity"] > 0) { ?>
-                            <button class="btn waves-effect waves-light" type="submit" name="action">
+                            <button class="btn waves-effect waves-light right" type="submit" name="action">
                                 Buy
                                 <i class="material-icons right">shopping_cart</i>
                             </button>
                         <?php  } else { ?>
-                        <button class="btn disabled waves-effect waves-light">
-                            Out of Stock
+                        <button class="btn disabled waves-effect waves-light right">
+                            No Stock
                             <i class="material-icons right">shopping_cart</i>
                         </button>
                     <?php  } ?>
-                    </td>
                 </tr>
-            </table>
+            </div>
+          </div>
+
         </div>
     </form>
            <?php 
@@ -74,7 +84,11 @@
 ?>
 
 <script>
-$(document).ready(function(){
-    $('.materialboxed').materialbox();
-});
+
+        var addToCart = function addToCart(event) {
+            var itemId = $(event.target).parents('form').find('.item-id-input').val();
+            $.post('cart.php', {'itemId': itemId}, function(data) {
+                console.log(data);
+            });
+        }
 </script>
