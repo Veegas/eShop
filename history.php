4c796159bc884c67408422dbc485254877a19629
nelsammak <nadah.125@gmail.com>
2015-10-02 19:02:19 +0200

    history supposedly done

--- history.php
+++ history.php
@@ -4,7 +4,6 @@
 	require_once(TEMPLATES_PATH . "/header.php");
 ?>
 
-<div class = "section" id = ""> 
 	<?php
 		$query = "SELECT Product.photo, Product.product_name, Product.description, Product.price, 
 					User_Product_Purchase.date, User_Product_Purchase.quantity 
@@ -12,18 +11,37 @@
 		$result = mysqli_query($conn, $query);
 		if ($result) {
 			while ($row = mysqli_fetch_assoc($result)){
-				echo $row["photo"].$row["product_name"].$row["date"]. "<br>" 
-					.$row["description"]. "<br>" .$row["quantity"].$row["price"]."<br>";		//	printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
+	?>
+				<table>
+		        <thead>
+		          <tr>
+		          	
+		              <th data-field="photo"></th>
+		              <th data-field="name">Item Name</th>
+		              <th data-field="description">Description</th>
+		              <th data-field="date">Date</th>
+		              <th data-field="price">Item Price</th>
+		              <th data-field="quantity">Quantity</th>
+		              <th data-field="total_p">Total</th>
+		          </tr>
+		        </thead>
+
+		        <tbody>
+		          <tr>
+		            <td><img src="<?php echo $row['photo']; ?>" alt="" width="100%" height="auto"></td>
+		            <td><?php echo $row["photo"]?></td>
+		            <td><?php echo $row["product_name"]?></td>
+		            <td><?php echo $row["description"]?></td>
+		            <td><?php echo $row["date"]?></td>
+		            <td><?php echo $row["price"]?></td>
+		            <td><?php echo $row["quantity"]?></td>
+		          </tr>
+				
+						
 			}
 		}
 
-	?>
-</div>
-
-<div class="divider"></div>
-
 <a class="waves-effect waves-light btn"><i class="material-icons left">attach_money</i>Buy</a>
 <?php
-
 	require_once(TEMPLATES_PATH . "/footer.php");
 ?>
