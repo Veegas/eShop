a638928c95142ee7a609f0af6c3d13b12fd6a680
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-10-03 00:46:42 +0200

    Removed shop

--- index.php
+++ index.php
@@ -1,44 +1,108 @@
-
-<?php
-
-$mysqli = mysqli_init();
-if (!$mysqli) {
-    die('mysqli_init failed');
-}
-
-if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
-    die('Setting MYSQLI_INIT_COMMAND failed');
-}
-
-if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
-    die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
-}
-
-if (!$mysqli->real_connect('localhost', 'eShop')) {
-    die('Connect Error (' . mysqli_connect_errno() . ') '
-            . mysqli_connect_error());
-}
-
-echo 'Success... ' . $mysqli->host_info . "\n";
-
- function __destruct()
-{
-   mysqli_close($this->connection);
-}
-?>
-
 <?php    
     // load up your config file
     require_once("./resources/config.php");
-     
+         session_start();
+
     require_once(TEMPLATES_PATH . "/header.php");
+    // require_once(TEMPLATES_PATH . "/authentication.php");
 ?>
 
-<div class ="container">
+<script>
+    var signIn = function signIn(event) {
+            $("#modal1").openModal();
+    }
+</script>
 
-          
+<div class ="container">
+    <div class="row products-row">
+        
+  <?php 
+
+        $query = "SELECT * FROM Product ";
+        $result = mysqli_query($conn, $query);
+    if ($result) {
+            // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+?>  
+    <form class="buy-form" action="buy.php" method="post" onsubmit=<?php if (!isset($_SESSION["user"])) { echo "'event.preventDefault(); signIn();'"; } ?>>
+        <div class="col s4 md4 l4 ">
+        <input class="item-id-input" type="text" name="item-id" value="<?php echo $row["ID"]; ?>" hidden>
+
+            <div class="card products-item">
+            <div class="card-image">
+            <img class="activator products-img" data-caption="<?php  echo $row["description"] ?>" src="<?php echo $row['photo']; ?>" alt="" >
+            </div>
+
+            <div class="card-content ">
+              <span class="card-title black-text"> <?php echo $row["product_name"] ?> </span>
+              <span class="card-title black-text right"> <?php echo $row["price"]; ?>$ </span>
+            </div>
+
+            <div class="card-reveal">
+              <span class="card-title black-text"> <?php echo $row["product_name"] ?> </span>
+              <p> <?php echo $row["description"] ?> </p>
+                                
+            </div>
+            <div class="card-action">
+                    <tr>
+                    <td class="left">
+                            <button class="btn-floating waves-effect waves-light left cart-btn" type="button" onclick="addToCart(event);">
+                                <i class="material-icons right">add_shopping_cart</i>
+                            </button>
+                            <button class="btn-floating waves-effect waves-light left remove-cart-btn deep-orange" type="button" onclick="removeFromCart(event);" style="display: none" >
+                                <i class="material-icons right">remove_circle_outline</i>
+                            </button>
+                    </td>
+
+                        <?php if ($row["quantity"] > 0) { ?>
+                            <button class="btn waves-effect waves-light right" type="submit" name="action">
+                                Buy
+                                <i class="material-icons right">shopping_cart</i>
+                            </button>
+                        <?php  } else { ?>
+                        <button class="btn disabled waves-effect waves-light right">
+                            No Stock
+                            <i class="material-icons right">shopping_cart</i>
+                        </button>
+                    <?php  } ?>
+                </tr>
+            </div>
+          </div>
+
+        </div>
+    </form>
+           <?php 
+        }
+    } else {
+        echo "No results";
+    }
+     ?> 
+
+
+    </div>
 </div>
 
 <?php
     require_once(TEMPLATES_PATH . "/footer.php");
 ?>
+
+<script>
+
+        var addToCart = function addToCart(event) {
+            var itemId = $(event.target).parents('form').find('.item-id-input').val();
+            $.post('cart.php', {'itemId': itemId}, function(data) {
+                $(event.target).parents('form').find('.cart-btn').hide();
+                $(event.target).parents('form').find('.remove-cart-btn').show();
+            });
+        }
+
+        var removeFromCart = function removeFromCart (event) {
+            var itemId = $(event.target).parents('form').find('.item-id-input').val();
+            $.post('removeCart.php', {'itemId': itemId}, function(data) {
+                console.log(data);
+                $(event.target).parents('form').find('.remove-cart-btn').hide();
+                $(event.target).parents('form').find('.cart-btn').show();
+            });
+            
+        }
+</script>
