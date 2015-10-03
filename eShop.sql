01ecc484afb59d82eb3aaaaacb04f0fb7a1b21ad
nelsammak <nadah.125@gmail.com>
2015-10-03 10:06:39 +0200

    Final product

--- eShop.sql
+++ eShop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 03, 2015 at 03:36 AM
+-- Generation Time: Oct 03, 2015 at 10:05 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -33,17 +33,22 @@ CREATE TABLE IF NOT EXISTS `Product` (
   `ID` int(11) NOT NULL,
   `photo` text,
   `product_name` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Product`
 --
 
 INSERT INTO `Product` (`price`, `quantity`, `description`, `ID`, `photo`, `product_name`) VALUES
-(200, 9, 'WOW', 1, 'img/feather.svg', 'Farkha'),
-(200, 7, 'OMG', 2, 'img/chicken-hat.png', 'Deek'),
-(300, 7, 'Best Ducks in Town', 3, 'img/duck-hat.jpg', 'Duck'),
-(10, 9, 'Agwad anwa3 el ferakh', 4, 'img/chick-hat.jpg', 'Chick');
+(200, 6, 'Agwad anwa3 el ferakh. Es2al bara', 1, 'img/chicken-hat.png', 'Chicken'),
+(200, 0, 'El deek beydan', 2, 'img/rooster-hat.jpg', 'Deek'),
+(30, 1, 'Best Ducks in Town', 3, 'img/duck-hat.jpg', 'Duck'),
+(45, 8, 'Just married.', 4, 'img/chick-hat.jpg', 'Bride and Groom Chicks'),
+(500, 20, 'Badass swan on a beach', 5, 'img/swan-glass.jpg', 'Swan'),
+(150, 50, 'Sophisticated Ostrich wearing a classy hat', 6, 'img/ostrich-hat.jpg', 'Ostrich'),
+(10, 20, 'Just a normal chick with no super powers.', 7, 'img/chick-classic.jpg', 'Classic Chick'),
+(100, 15, 'Fancy Nancy chick.', 8, 'img/chick-fancy.jpg', 'Fancy Chick'),
+(43, 18, 'Chick grows into Hulk', 9, 'img/chick-green.jpg', 'Chick Hulk');
 
 -- --------------------------------------------------------
 
@@ -58,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `User` (
   `first_name` text NOT NULL,
   `last_name` text NOT NULL,
   `avatar` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `User`
@@ -66,7 +71,8 @@ CREATE TABLE IF NOT EXISTS `User` (
 
 INSERT INTO `User` (`email`, `ID`, `password`, `first_name`, `last_name`, `avatar`) VALUES
 ('test@test.com', 1, '*94BDCEBE19083CE2A1F959FD02F964C7AF4CFC29', 'test', 'test', '/img/cicken-hat.jpg'),
-('test2@test.com', 2, '*7CEB3FDE5F7A9C4CE5FBE610D7D8EDA62EBE5F4E', 'Test', '2nd', '');
+('test2@test.com', 2, '*7CEB3FDE5F7A9C4CE5FBE610D7D8EDA62EBE5F4E', 'Test', '2nd', ''),
+('test1@test.com', 3, 'test', 'test2', 'test', '');
 
 -- --------------------------------------------------------
 
@@ -87,7 +93,12 @@ CREATE TABLE IF NOT EXISTS `User_Product_Purchase` (
 
 INSERT INTO `User_Product_Purchase` (`product_id`, `user_id`, `date`, `quantity`) VALUES
 (2, 1, '2015-10-02 17:12:46', 2),
-(4, 1, '2015-10-02 17:12:46', 1);
+(4, 1, '2015-10-02 17:12:46', 1),
+(1, 3, '2015-10-03 07:42:12', 1),
+(1, 3, '2015-10-03 07:43:29', 1),
+(2, 3, '2015-10-03 07:43:29', 1),
+(3, 3, '2015-10-03 07:43:29', 1),
+(4, 3, '2015-10-03 07:43:29', 1);
 
 --
 -- Indexes for dumped tables
@@ -120,12 +131,12 @@ ALTER TABLE `User_Product_Purchase`
 -- AUTO_INCREMENT for table `Product`
 --
 ALTER TABLE `Product`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
