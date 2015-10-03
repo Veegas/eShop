fe956317b2582e4f1e20736cc295813ad790c453
nelsammak <nadah.125@gmail.com>
2015-10-03 03:44:22 +0200

    insertion into user_product_purchase upon checkout in buy.php and dummy data for trsting in .sql

--- buy.php
+++ buy.php
@@ -14,7 +14,9 @@ print_r($_POST);
 	foreach ($cart_ids as $p_id) {
 		$query = "UPDATE `Product` SET `quantity` = quantity - 1 WHERE `Product`.`ID` = " . $p_id ;
 		$result = mysqli_query($conn , $query);
-		echo $result;		
+		echo $result;	
+		$query2 = "INSERT INTO `User_Product_Purchase` (`product_id`, `user_id`, `quantity`) VALUES
+			($p_id, $_SESSION["user"], 1);"
 	}
 	
 
